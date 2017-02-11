167ff51740fff5f096962a8e5e9fad24b341dcbc
eep10 <eep10@10.183.2.167>
2017-02-11 10:32:42 -0600

    finished subsystem added enable/disable hopper deleted middle CANTalon

--- src/org/usfirst/frc/team2526/robot/subsystems/Hopper.java
+++ src/org/usfirst/frc/team2526/robot/subsystems/Hopper.java
@@ -1,23 +1,26 @@
 package org.usfirst.frc.team2526.robot.subsystems;
-
-import com.ctre.CANTalon;
-
-import edu.wpi.first.wpilibj.buttons.JoystickButton;
-
-import edu.wpi.first.wpilibj.command.Subsystem;
-
-public class Hopper extends Subsystem {
-	CANTalon top;
-	CANTalon middle;
-	CANTalon bottom;
 	
-	public Hopper(int topID, int middleID, int bottomID){
+import com.ctre.CANTalon;
+import edu.wpi.first.wpilibj.command.Subsystem;
+	
+public class Hopper extends Subsystem {
+	private CANTalon top;
+	private CANTalon bottom;
+	
+	public Hopper(int topID, int bottomID){
 		top = new CANTalon(topID);
-		middle = new CANTalon(middleID);
 		bottom = new CANTalon(bottomID);
 	}
 	@Override
 	protected void initDefaultCommand() {
 		// TODO Auto-generated method stub
+	}
+	public void enableHopper(){
+		top.set(-.5);
+		bottom.set(.5);
+	}
+	public void disableHopper(){
+		top.set(0);
+		bottom.set(0);
 	}
 }
