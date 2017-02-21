ada5f6e419fda5fea543a0f502384e8d2a147d98
Calvin Jou <calvin@calvinjou.xyz>
2017-02-21 16:53:57 -0600

    Instantiate OI last

--- src/org/usfirst/frc/team2526/robot/Robot.java
+++ src/org/usfirst/frc/team2526/robot/Robot.java
@@ -31,8 +31,6 @@ public class Robot extends IterativeRobot {
 	public static GearIntake gearintake = new GearIntake(RobotMap.DS_L_ONE, RobotMap.DS_L_TWO, RobotMap.DS_R_ONE, RobotMap.DS_R_TWO, RobotMap.SS_P, RobotMap.D_G_S);
 	//Turret Subsystem
 	public static final Turret turret = new Turret(RobotMap.TURRET_TALON, RobotMap.GAINS_TURRET);
-	//Controls
-	public static OI oi = new OI();
 	//BallIntake subsystem
 	public static final BallIntake intake = new BallIntake(RobotMap.INTAKE);
 	//DriveTrain Subsystem
@@ -43,6 +41,8 @@ public class Robot extends IterativeRobot {
 	public static final Flywheel flywheel = new Flywheel(RobotMap.FLYWHEEL_TALON, RobotMap.FLYWHEEL_TALON_FOLLOWER, RobotMap.GAINS_FLYWHEEL);
 	//Elevator Subsystem
 	public static final Elevator elevator = new Elevator(RobotMap.ELEVATOR_BOTTOM, RobotMap.ELEVATOR_TOP, RobotMap.ELEVATOR_PID_GAINS);
+	//Controls
+	public static OI oi = new OI();
 	//AutonomousCommand
 	Command autonomousCommand;
 	SendableChooser<Command> chooser = new SendableChooser<>();
