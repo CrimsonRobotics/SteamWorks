de6fef39207a394ea3e589762476fc11a3dbf885
Calvin Jou <calvin@calvinjou.xyz>
2017-02-21 21:40:54 -0600

    Change mapped values

--- src/org/usfirst/frc/team2526/robot/OI.java
+++ src/org/usfirst/frc/team2526/robot/OI.java
@@ -5,6 +5,8 @@ import org.usfirst.frc.team2526.robot.commands.GameSelectorGear;
 import org.usfirst.frc.team2526.robot.commands.GearIntakeDrop;
 import org.usfirst.frc.team2526.robot.commands.IntakeCommand;
 import org.usfirst.frc.team2526.robot.commands.RunElevator;
+import org.usfirst.frc.team2526.robot.commands.RunFlywheel;
+
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.buttons.Button;
 import edu.wpi.first.wpilibj.buttons.JoystickButton;
@@ -16,18 +18,20 @@ public class OI {
 	private Joystick driverLeft = new Joystick(0);
 	private Joystick driverRight = new Joystick(1);
 	private Joystick coDriver = new Joystick(2);
-	private Button GearDrop = new JoystickButton(coDriver, 1);
+	//private Button GearDrop = new JoystickButton(coDriver, 1);
 	private Button SwitchToBall = new JoystickButton(coDriver, 2);
 	private Button SwitchToGear = new JoystickButton(coDriver, 3);
 	private Button intake = new JoystickButton(driverRight, 1);
 	private Button elevator = new JoystickButton(driverLeft, 2);
+	private Button flywheel = new JoystickButton(coDriver, 1);
 	
 	public OI(){
-		GearDrop.whileHeld(new GearIntakeDrop());
+		//GearDrop.whileHeld(new GearIntakeDrop());
 		SwitchToBall.whenPressed(new GameSelectorBalls());
 		SwitchToGear.whenPressed(new GameSelectorGear());
-		intake.whenPressed(new IntakeCommand());
+		intake.whileHeld(new IntakeCommand());
 		elevator.whileHeld(new RunElevator(RobotMap.ELEVATOR_SPEED));
+		flywheel.whileHeld(new RunFlywheel(RobotMap.FLYWHEEL_BASE_SPEED));
 	}
 	public Joystick getDriverLeft(){
 		return driverLeft;
--- src/org/usfirst/frc/team2526/robot/RobotMap.java
+++ src/org/usfirst/frc/team2526/robot/RobotMap.java
@@ -40,6 +40,7 @@ public class RobotMap {
 	public static final int FLYWHEEL_TALON = 9;
 	public static final int FLYWHEEL_TALON_FOLLOWER = 8;
 	public static final PID GAINS_FLYWHEEL = new PID(0.29,0.0001,10,0.035,0,0,1);//.29,.0001,10,0.035,0,0
+	public static final int FLYWHEEL_BASE_SPEED = 3000; //RPM
 	/*
 	 * Gear placer Solenoids, PCM, and switch
 	 */
