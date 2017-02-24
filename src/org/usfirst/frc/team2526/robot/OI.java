906b6ed6a305b3023fa6f4b71111315d3dd9709d
Calvin Jou <calvin@calvinjou.xyz>
2017-02-24 15:35:35 -0600

    Add command group for codriver

--- src/org/usfirst/frc/team2526/robot/OI.java
+++ src/org/usfirst/frc/team2526/robot/OI.java
@@ -1,12 +1,12 @@
 package org.usfirst.frc.team2526.robot;
 
 import org.usfirst.frc.team2526.robot.commands.GameSelectorBalls;
-import org.usfirst.frc.team2526.robot.commands.GameSelectorGear;
 import org.usfirst.frc.team2526.robot.commands.GearIntakeDrop;
 import org.usfirst.frc.team2526.robot.commands.IntakeCommand;
 import org.usfirst.frc.team2526.robot.commands.RunElevator;
 import org.usfirst.frc.team2526.robot.commands.RunFlywheel;
 import org.usfirst.frc.team2526.robot.commands.StopTurret;
+import org.usfirst.frc.team2526.robot.commands.groups.LoadAndBallMode;
 
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.buttons.Button;
@@ -16,27 +16,37 @@ import edu.wpi.first.wpilibj.buttons.JoystickButton;
  * interface to the commands and command groups that allow control of the robot.
  */
 public class OI {
+	/*
+	 * DRIVER CONTROLS
+	 */
 	private Joystick driverLeft = new Joystick(0);
 	private Joystick driverRight = new Joystick(1);
+	private Button highGear = new JoystickButton(driverLeft, 2);
+	/*
+	 * CO DRIVER CONTROLS
+	 */
 	private Joystick coDriver = new Joystick(2);
 	private Button GearDrop = new JoystickButton(coDriver, 10);
-	private Button SwitchToBall = new JoystickButton(coDriver, 2);
-	private Button SwitchToGear = new JoystickButton(coDriver, 3);
+	private Button SwitchToBall = new JoystickButton(coDriver, 1);
 	private Button intake = new JoystickButton(driverRight, 1);
 	private Button elevator = new JoystickButton(driverLeft, 2);
 	private Button flywheel = new JoystickButton(coDriver, 1);
 	private Button stopTurret = new JoystickButton(coDriver,12);
 	
 	public OI(){
+		/*
+		 * DRIVER COMMANDS
+		 */
+		//highGear.whileHeld(command);
+		/*
+		 * CO DRIVER COMMANDS
+		 */
 		GearDrop.whileHeld(new GearIntakeDrop());
-		SwitchToBall.whenPressed(new GameSelectorBalls());
-		SwitchToGear.whenPressed(new GameSelectorGear());
+		SwitchToBall.whenPressed(new LoadAndBallMode());
 		intake.whileHeld(new IntakeCommand());
 		stopTurret.whenPressed(new StopTurret());
 		elevator.whileHeld(new RunElevator(RobotMap.ELEVATOR_SPEED));
 		flywheel.whileHeld(new RunFlywheel(RobotMap.FLYWHEEL_BASE_SPEED));
-		
-		
 	}
 	public Joystick getDriverLeft(){
 		return driverLeft;
--- src/org/usfirst/frc/team2526/robot/commands/GameSelectorBalls.java
+++ src/org/usfirst/frc/team2526/robot/commands/GameSelectorBalls.java
@@ -31,10 +31,12 @@ public class GameSelectorBalls extends Command {
 
     // Called once after isFinished returns true
     protected void end() {
+    	Robot.gearintake.switchtoGear();
     }
 
     // Called when another command which requires one or more of the same
     // subsystems is scheduled to run
     protected void interrupted() {
+    	Robot.gearintake.switchtoGear();
     }
 }
